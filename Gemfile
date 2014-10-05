643048b8771bc3ecb424413bc1dd3760a54a3019
robotbrain <robotbrainify@gmail.com>
2014-10-05 15:52:32 -0400

    Google and some work on main functionality

--- Gemfile
+++ Gemfile
@@ -2,7 +2,6 @@ source 'https://rubygems.org'
 
 gem 'cinch', '~>2.1.0'
 gem 'rack', '~>1.1'
-gem 'rspec', :require => 'spec'
 
 Dir['plugins/**/Gemfile'].each do |gemfile|
   self.send(:eval, File.open(gemfile, 'r').read)
--- plugins/info/info.rb
+++ plugins/info/info.rb
@@ -1,3 +1,16 @@
+require 'json'
 class Info
   include Cinch::Plugin
+  prefix '.'
+  match /([a-zA-Z]+)(.*)/
+  file = File.read('responses.json')
+  responses = JSON.parse(file)
+  def execute(m, command, args)
+    if not responses.has_key?(command) then
+      m.reply "Invalid command: #{command}"
+    else
+      resp = responses[command]
+      resp.gsub /\$\{list\}/, responses.keys.join ', '
+    end
+  end
 end
