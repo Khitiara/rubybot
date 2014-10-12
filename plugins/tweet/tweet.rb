41168e39d769ae01efb4f765946565c2dc15cc00
robotbrain <robotbrainify@gmail.com>
2014-10-12 09:45:15 -0400

    I am an idiot

--- plugins/tweet/tweet.rb
+++ plugins/tweet/tweet.rb
@@ -2,7 +2,7 @@ require 'twitter'
 
 $twitter = Twitter::REST::Client.new do |config|
   config.consumer_key = $config['twitter']['consumer_key']
-  config.consumer_secret = $config['twitter']['consumer_key']
+  config.consumer_secret = $config['twitter']['consumer_secret']
   config.access_token = $config['twitter']['access_key']
   config.access_token_secret = $config['twitter']['access_secret']
 end
