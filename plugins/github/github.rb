ba4cbc36203923426d287a5b7a9ed5f9d471e3e9
robotbrain <robotbrainify@gmail.com>
2014-11-05 18:18:41 -0500

    config

--- plugins/github/github.rb
+++ plugins/github/github.rb
@@ -1,10 +1,11 @@
 require_relative '../http/http_server'
+require 'gitio'
 
 class Github
   include Cinch::Plugin
   extend Cinch::HttpServer::Verbs
 
-  require 'gitio'
+  channels = $config['github_repos']
 
   before do
     request.body.rewind
@@ -22,22 +23,32 @@ class Github
         title = payload[:pull_request][:title]
         url = Gitio::shorten payload[:pull_request][:html_url]
         user = payload[:sender][:login]
-        bot.channels.each { |chan| chan.msg "[#{repo}]: #{user} #{action} pull request \##{issue}: \"#{title}\": #{url}" }
+        channels[payload[:repository][:full_name]].map! { |it| bot.channel_list.find it }.each { |chan| chan.msg "[#{repo}]: #{user} #{action} pull request \##{issue}: \"#{title}\": #{url}" }
 
       when 'pull_request_review_comment'
         url = Gitio::shorten payload[:comment][:html_url]
         issue = payload[:pull_request][:number]
         user = payload[:comment][:user][:login]
         repo = payload[:repository][:name]
-        bot.channels.each { |chan| chan.msg "[#{repo}]: #{user} reviewed pull request \##{issue}: #{url}" }
+        channels[payload[:repository][:full_name]].map! { |it| bot.channel_list.find it }.each { |chan| chan.msg "[#{repo}]: #{user} reviewed pull request \##{issue}: #{url}" }
 
       when 'push'
         name = payload[:ref]
-        num = payload[:size]
+        name.slice!(/^refs\/heads\//)
+        num = payload[:commits].length
         repo = payload[:repository][:name]
-        url = Gitio::shorten payload[:repository][:html_url]
+        url = Gitio::shorten payload[:compare]
         user = payload[:sender][:login]
-        bot.channels.each { |chan| chan.msg "[#{repo}]: #{user} pushed #{num} commits to #{name}: #{url}" }
+        channels[payload[:repository][:full_name]].map! { |it| bot.channel_list.find it }.each do |chan|
+          chan.msg "[#{repo}]: #{user} pushed #{num} commits to #{name}: #{url}"
+          payload[:commits].take(3).each do |commit|
+            chan.msg "[#{repo}]: #{commit[:message]}"
+          end
+          unless num - 3 < 0
+            chan.msg "[#{repo}]: ...and #{num - 3} more."
+          end
+        end
+
 
       when 'issues'
         action = payload[:action]
@@ -47,15 +58,16 @@ class Github
           title = payload[:issue][:title]
           url = Gitio::shorten payload[:issue][:html_url]
           user = payload[:sender][:login]
-          bot.channels.each { |chan| chan.msg "[#{repo}]: #{user} #{action} issue \##{issue}: \"#{title}\": #{url}" }
+          channels[payload[:repository][:full_name]].map! { |it| bot.channel_list.find it }.each { |chan| chan.msg "[#{repo}]: #{user} #{action} issue \##{issue}: \"#{title}\": #{url}" }
         end
 
       when 'issue_comment'
         url = Gitio::shorten payload[:issue][:html_url]
         issue = payload[:issue][:number]
         user = payload[:comment][:user][:login]
+        title = payload[:issue][:title]
         repo = payload[:repository][:name]
-        bot.channels.each { |chan| chan.msg "[#{repo}]: #{user} commented on issue \##{issue}: #{url}" }
+        channels[payload[:repository][:full_name]].map! { |it| bot.channel_list.find it }.each { |chan| chan.msg "[#{repo}]: #{user} commented on issue \##{issue}: \"#{title}\" #{url}" }
 
       when 'create'
         name = payload[:ref]
@@ -63,7 +75,7 @@ class Github
         repo = payload[:repository][:name]
         url = Gitio::shorten payload[:repository][:html_url]
         user = payload[:sender][:login]
-        bot.channels.each { |chan| chan.msg "[#{repo}]: #{user} created #{type} #{name}: #{url}" }
+        channels[payload[:repository][:full_name]].map! { |it| bot.channel_list.find it }.each { |chan| chan.msg "[#{repo}]: #{user} created #{type} #{name}: #{url}" }
 
       when 'delete'
         name = payload[:ref]
@@ -71,20 +83,20 @@ class Github
         repo = payload[:repository][:name]
         url = Gitio::shorten payload[:repository][:html_url]
         user = payload[:sender][:login]
-        bot.channels.each { |chan| chan.msg "[#{repo}]: #{user} deleted #{type} #{name}: #{url}" }
+        channels[payload[:repository][:full_name]].map! { |it| bot.channel_list.find it }.each { |chan| chan.msg "[#{repo}]: #{user} deleted #{type} #{name}: #{url}" }
 
       when 'fork'
         repo = payload[:repository][:name]
         url = Gitio::shorten payload[:forkee][:html_url]
         user = payload[:forkee][:owner][:login]
-        bot.channels.each { |chan| chan.msg "[#{repo}]: #{user} forked the repo: #{url}" }
+        channels[payload[:repository][:full_name]].map! { |it| bot.channel_list.find it }.each { |chan| chan.msg "[#{repo}]: #{user} forked the repo: #{url}" }
 
       when 'commit_comment'
         url = Gitio::shorten payload[:comment][:html_url]
         commit = payload[:comment][:commit_id]
         user = payload[:comment][:user][:login]
         repo = payload[:repository][:name]
-        bot.channels.each { |chan| chan.msg "[#{repo}]: #{user} commented on commit #{commit}: #{url}" }
+        channels[payload[:repository][:full_name]].map! { |it| bot.channel_list.find it }.each { |chan| chan.msg "[#{repo}]: #{user} commented on commit #{commit}: #{url}" }
       else
         #bot.channel_list.find('#ElrosBot').msg "unrecognized!: #{payload}"
     end
