68f7c3ee4f90b445b4e5935d9b1c9d900210c4a3
robotbrain <robotbrainify@gmail.com>
2014-11-16 10:57:34 -0500

    Fixes I hope

--- plugins/github/github.rb
+++ plugins/github/github.rb
@@ -24,14 +24,14 @@ class Github
         title = payload[:pull_request][:title]
         url = Gitio::shorten payload[:pull_request][:html_url]
         user = payload[:sender][:login]
-        channels[payload[:repository][:full_name]].map { |it| bot.channel_list.find it }.each { |chan| chan.msg "[#{Format(:blue, repo)}]: #{user} #{action} pull request \##{Format(:green, issue)}: \"#{title}\" - #{Format(:orange, url)}" }
+        channels[payload[:repository][:full_name]].map { |it| bot.channel_list.find it }.each { |chan| chan.msg "[#{Format(:blue, repo)}]: #{user} #{action} pull request \##{Format(:green, issue)}: \"#{title}\" - #{url}" }
 
       when 'pull_request_review_comment'
         url = Gitio::shorten payload[:comment][:html_url]
         issue = payload[:pull_request][:number]
         user = payload[:comment][:user][:login]
         repo = payload[:repository][:name]
-        channels[payload[:repository][:full_name]].map { |it| bot.channel_list.find it }.each { |chan| chan.msg "[#{Format(:blue, repo)}]: #{Format(:yellow, user)} reviewed pull request \##{Format(:green, issue)} - #{Format(:orange, url)}" }
+        channels[payload[:repository][:full_name]].map { |it| bot.channel_list.find it }.each { |chan| chan.msg "[#{Format(:blue, repo)}]: #{Format(:yellow, user)} reviewed pull request \##{Format(:green, issue)} - #{url}" }
 
       when 'push'
         name = payload[:ref]
@@ -41,7 +41,7 @@ class Github
         url = Gitio::shorten payload[:compare]
         user = payload[:sender][:login]
         channels[payload[:repository][:full_name]].map { |it| bot.channel_list.find it }.each do |chan|
-          chan.msg "[#{Format(:blue, repo)}]: #{Format(:yellow, user)} pushed #{Format(:green, num)} commits to #{Format(:green, name)}: #{Format(:orange, url)}"
+          chan.msg "[#{Format(:blue, repo)}]: #{Format(:yellow, user)} pushed #{Format(:green, num)} commits to #{Format(:green, name)}: #{url}"
           payload[:commits].take(3).each do |commit|
             chan.msg "[#{repo}]: #{commit[:message]}"
           end
@@ -59,7 +59,7 @@ class Github
           title = payload[:issue][:title]
           url = Gitio::shorten payload[:issue][:html_url]
           user = payload[:sender][:login]
-          channels[payload[:repository][:full_name]].map { |it| bot.channel_list.find it }.each { |chan| chan.msg "[#{Format(:blue, repo)}]: #{Format(:yellow, user)} #{action} issue \##{Format(:green, issue)}: \"#{title}\" - #{Format(:orange, url)}" }
+          channels[payload[:repository][:full_name]].map { |it| bot.channel_list.find it }.each { |chan| chan.msg "[#{Format(:blue, repo)}]: #{Format(:yellow, user)} #{action} issue \##{Format(:green, issue)}: \"#{title}\" - #{url}" }
         end
 
       when 'issue_comment'
@@ -68,7 +68,7 @@ class Github
         user = payload[:comment][:user][:login]
         title = payload[:issue][:title]
         repo = payload[:repository][:name]
-        channels[payload[:repository][:full_name]].map { |it| bot.channel_list.find it }.each { |chan| chan.msg "[#{Format(:blue, repo)}]: #{user} commented on issue \##{Format(:green, issue)}: \"#{title}\" - #{Format(:orange, url)}" }
+        channels[payload[:repository][:full_name]].map { |it| bot.channel_list.find it }.each { |chan| chan.msg "[#{Format(:blue, repo)}]: #{user} commented on issue \##{Format(:green, issue)}: \"#{title}\" - #{url}" }
 
       when 'create'
         name = payload[:ref]
@@ -76,7 +76,7 @@ class Github
         repo = payload[:repository][:name]
         url = Gitio::shorten payload[:repository][:html_url]
         user = payload[:sender][:login]
-        channels[payload[:repository][:full_name]].map { |it| bot.channel_list.find it }.each { |chan| chan.msg "[#{Format(:blue, repo)}]: #{Format(:yellow, user)} created #{type} #{name}: #{Format(:orange, url)}" }
+        channels[payload[:repository][:full_name]].map { |it| bot.channel_list.find it }.each { |chan| chan.msg "[#{Format(:blue, repo)}]: #{Format(:yellow, user)} created #{type} #{name}: #{url}" }
 
       when 'delete'
         name = payload[:ref]
@@ -84,20 +84,20 @@ class Github
         repo = payload[:repository][:name]
         url = Gitio::shorten payload[:repository][:html_url]
         user = payload[:sender][:login]
-        channels[payload[:repository][:full_name]].map { |it| bot.channel_list.find it }.each { |chan| chan.msg "[#{Format(:blue, repo)}]: #{Format(:yellow, user)} deleted #{type} #{name}: #{Format(:orange, url)}" }
+        channels[payload[:repository][:full_name]].map { |it| bot.channel_list.find it }.each { |chan| chan.msg "[#{Format(:blue, repo)}]: #{Format(:yellow, user)} deleted #{type} #{name}: #{url}" }
 
       when 'fork'
         repo = payload[:repository][:name]
         url = Gitio::shorten payload[:forkee][:html_url]
         user = payload[:forkee][:owner][:login]
-        channels[payload[:repository][:full_name]].map! { |it| bot.channel_list.find it }.each { |chan| chan.msg "[#{Format(:blue, repo)}]: #{Format(:yellow, user)} forked the repo: #{Format(:orange, url)}" }
+        channels[payload[:repository][:full_name]].map! { |it| bot.channel_list.find it }.each { |chan| chan.msg "[#{Format(:blue, repo)}]: #{Format(:yellow, user)} forked the repo: #{url}" }
 
       when 'commit_comment'
         url = Gitio::shorten payload[:comment][:html_url]
         commit = payload[:comment][:commit_id]
         user = payload[:comment][:user][:login]
         repo = payload[:repository][:name]
-        channels[payload[:repository][:full_name]].map { |it| bot.channel_list.find it }.each { |chan| chan.msg "[#{Format(:blue, repo)}: #{Format(:yellow, user)} commented on commit #{Format(:green, commit)}: #{Format(:orange, url)}" }
+        channels[payload[:repository][:full_name]].map { |it| bot.channel_list.find it }.each { |chan| chan.msg "[#{Format(:blue, repo)}: #{Format(:yellow, user)} commented on commit #{Format(:green, commit)}: #{url}" }
       else
         #bot.channel_list.find('#ElrosBot').msg "unrecognized!: #{payload}"
     end
--- plugins/google/google.rb
+++ plugins/google/google.rb
@@ -4,6 +4,19 @@ require 'cinch/formatting'
 
 $bold_regex = /<b(\b.*)?>(?<bold>.*)<\/b>/
 $italics_regex = /<i(\b.*)?>(?<italic>.*)<\/i>/
+$formatter = lambda do |env|
+  node = env[:node]
+  content = node.content
+  case env[:node_name]
+    when 'i'
+      out = Format(:italic, content)
+    when 'b'
+      out = Format(:bold, content)
+    else
+      out = content
+  end
+  node.replace out
+end
 
 class Google
   include Cinch::Plugin
@@ -14,15 +27,7 @@ class Google
     GoogleAjax.referrer = 'cadwallion.com'
     result = GoogleAjax::Search.web(query)[:results][0]
     title = result[:title]
-    title = Sanitize.fragment(title, :elements => %w(b i))
-    title.gsub!($bold_regex) do |_|
-      m = $bold_regex.match(title)
-      Format(:bold, m['bold'])
-    end
-    title.gsub!($italics_regex) do |_|
-      m = $italics_regex.match(title)
-      Format(:italic, m['italic'])
-    end
+    title = Sanitize.fragment(title, :elements => %w(b i), :transformers => [$formatter])
     m.reply "Result: #{title} - #{result[:url]}"
   end
 end
