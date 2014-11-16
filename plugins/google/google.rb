e987281b5d7ff44b17cc0201d80b220262d0bbb3
robotbrain <robotbrainify@gmail.com>
2014-11-16 10:43:09 -0500

    Fix for attributes

--- plugins/google/google.rb
+++ plugins/google/google.rb
@@ -2,8 +2,8 @@ require 'googleajax'
 require 'sanitize'
 require 'cinch/formatting'
 
-$bold_regex = /<b>(.*)<\/b>/
-$italics_regex = /<i>(.*)<\/i>/
+$bold_regex = /<b(\b.*)?>(?<bold>.*)<\/b>/
+$italics_regex = /<i(\b.*)?>(?<italic>.*)<\/i>/
 
 class Google
   include Cinch::Plugin
@@ -16,10 +16,12 @@ class Google
     title = result[:title]
     title = Sanitize.fragment(title, :elements => %w(b i))
     title.gsub!($bold_regex) do |_|
-      Format(:bold, $1)
+      m = $bold_regex.match(title)
+      Format(:bold, m['bold'])
     end
     title.gsub!($italics_regex) do |_|
-      Format(:italic, $1)
+      m = $italics_regex.match(title)
+      Format(:italic, m['italic'])
     end
     m.reply "Result: #{title} - #{result[:url]}"
   end
