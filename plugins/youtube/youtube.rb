50ab7cfa3cd3448c882314c6766521f69c2871e3
robotbrain <robotbrain@robotbrain.info>
2015-05-09 11:06:45 -0400

    Add youtube thing

---
+++ plugins/youtube/youtube.rb
@@ -0,0 +1,14 @@
+require 'video_info'
+require 'chronic_duration'
+class Youtube
+  include Cinch::Plugin
+  YT_REGEX = /(http:\/\/(?:www\.)?youtube.com\/watch\?(?=.*v=\w+)(?:\S+)?)/
+  match YT_REGEX, use_prefix: false
+
+  def execute(m, link)
+    vid   = VideoInfo.new(link)
+    title = vid.title
+    dur   = ChronicDuration.output(vid.duration, :keep_zero => true)
+    m.reply "Youtube: #{title} (#{dur})"
+  end
+end
